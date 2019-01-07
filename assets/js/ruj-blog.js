42a0531f573ee7a6ec22093170481d23987f38de
chenruijun <chenruijun@bailitop.com>
2019-01-07 19:35:14 +0800

    blog add

--- assets/js/ruj-blog.js
+++ assets/js/ruj-blog.js
@@ -2,41 +2,15 @@
  * crj Blog v1.0.0 (http://rujpro.github.io)
  * Copyright 2018 crj luckyerchen@gmail.com
  */
-// Drop Bootstarp low-performance Navbar
-var $body   = document.body;
-var $toggle = document.querySelector('.navbar-toggle');
-var $navbar = document.querySelector('#rujblog_navbar');
-var $collapse = document.querySelector('.navbar-collapse');
-
-var __RujNav__ = {
-    close: function(){
-        $navbar.className = " ";
-        // wait until animation end.
-        setTimeout(function(){
-            // prevent frequently toggle
-            if($navbar.className.indexOf('in') < 0) {
-                $collapse.style.height = "0px"
-            }
-        },400)
-    },
-    open: function(){
-        $collapse.style.height = "auto"
-        $navbar.className += " in";
-    }
-};
-// Bind Event
-$toggle.addEventListener('click', function(e){
-    if ($navbar.className.indexOf('in') > 0) {
-        __RujNav__.close()
-    }else{
-        __RujNav__.open()
-    }
-});
-document.addEventListener('click', function(e){
-    if(e.target == $toggle) return;
-    if(e.target.className == 'icon-bar') return;
-    __RujNav__.close();
-});
+//<!-- async load function -->
+function async(u, c) {
+    var d = document, t = 'script',
+        o = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+    o.src = u;
+    if (c) { o.addEventListener('load', function (e) { c(null, e); }, false); }
+    s.parentNode.insertBefore(o, s);
+  }
 
 // <!-- canvas + h5 + webgl 水波纹效果 -->
 $(document).ready(function () {
@@ -79,3 +53,44 @@ var classname = document.getElementsByClassName("bubbly-button");
 for (var i = 0; i < classname.length; i++) {
     classname[i].addEventListener('click', animateButton, false);
 }
+// Navigation Scripts to Show Header on Scroll-Up
+$(document).ready(function() {
+    var MQL = 1170;
+
+    //primary navigation slide-in effect
+    if ($(window).width() > MQL) {
+        var headerHeight = $('.navbar-custom').height(),
+            bannerHeight  = $('.intro-header .container').height();     
+        $(window).on('scroll', {
+                previousTop: 0
+            },
+            function() {
+                var currentTop = $(window).scrollTop(),
+                    $catalog = $('.side-catalog');
+
+                //check if user is scrolling up by mouse or keyborad
+                if (currentTop < this.previousTop) {
+                    //if scrolling up...
+                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                        $('.navbar-custom').addClass('is-visible');
+                    } else {
+                        $('.navbar-custom').removeClass('is-visible is-fixed');
+                    }
+                } else {
+                    //if scrolling down...
+                    $('.navbar-custom').removeClass('is-visible');
+                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                }
+                this.previousTop = currentTop;
+
+
+                //adjust the appearance of side-catalog
+                $catalog.show()
+                if (currentTop > (bannerHeight + 41)) {
+                    $catalog.addClass('fixed')
+                } else {
+                    $catalog.removeClass('fixed')
+                }
+            });
+    }
+});

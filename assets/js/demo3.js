16f172b908aa9c79b473dea332b08066d791bb90
rujpro <837669279@qq.com>
2019-01-09 00:25:23 +0800

    样式调整

--- assets/js/demo3.js
+++ assets/js/demo3.js
@@ -15,16 +15,20 @@
 		closeCtrl = document.getElementById('btn-search-close'),
 		searchContainer = document.querySelector('.search'),
 		inputSearch = searchContainer.querySelector('.search__input');
+		$body = document.body;
 	function openSearch() {
 		mainContainer.classList.add('main-wrap--move');
 		searchContainer.classList.add('search--open');
 		inputSearch.focus();
+		$body.classList.add('mainbody');
 	}
 	function closeSearch() {
 		mainContainer.classList.remove('main-wrap--move');
 		searchContainer.classList.remove('search--open');
 		inputSearch.blur();
 		inputSearch.value = '';
+		$body.style.background = "";
+		$body.classList.remove('mainbody');
 	}
 	function initEvents() {
 		openCtrl.addEventListener('click', openSearch);
